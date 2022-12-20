312d36984ae63ea79c728c60f31b44f35ca38bf8
llucarz <115527911+llucarz@users.noreply.github.com>
2022-12-20 18:08:32 +0100

    V.1
    Ajout du site

---
+++ script.js
@@ -0,0 +1,47 @@
+let solution='indochine'
+let $=function (id){
+    return window.document.getElementById(id);
+};
+
+const toogle=(elementId)=>{
+    let elm=$(elementId);
+    if(elm.style.display==='none'){
+        elm.style.display='';
+    }
+    else{
+        elm.style.display='none';
+    }
+};
+$('bt-start').addEventListener('click', function () {
+    const proposition = $('proposition').value;
+    if (proposition.toLowerCase() === solution) {
+        toogle("step2")
+        toogle("step1")
+
+        return;
+    }
+    displayReponse('Non, c\'est pas ça !')
+})
+let displayReponse=function (message){
+    $('reponse').innerHTML=message
+}
+
+$('proposition').addEventListener('change', function (){
+        let proposition = $('proposition').value;
+
+})
+
+function OuvertureCadeau() {
+    window.open("beauregard.pdf"); }
+
+
+
+
+
+
+
+// tu avais mis step1 & 2 dans une class dans le HTML alors qu'il fallait un id
+
+
+
+
